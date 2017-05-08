6d1e1a76a0b3f5508d18e12bbe2a309f7f1836f4
Juliana Fortes <juliana.sameiro@g.xarevision.pt>
2017-05-08 16:01:03 +0100

    carousel1

--- js/main.js
+++ js/main.js
@@ -48,13 +48,41 @@ $(document).ready(function(){
         }, 1000);
     });
     
-    $("a[href^='#inicio']").on('click', function(e) {
+    $("a[href^='#myCarousel_inicio']").on('click', function(e) {
         e.preventDefault();
         hash = this.hash.split('#');
         $('html, body').animate({
             scrollTop: $('#' + hash[1]).offset().top
         }, 1000);
     });
+});
+
+
+/*** Carousel ***/
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image' : 'url(' + $src + ')',
+        'background-color' : $color
+    });
+    $(this).remove();
+});
+
+$(window).on('resize', function (){
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+    interval: 600000,
+    pause: "false"
 });
 
 
